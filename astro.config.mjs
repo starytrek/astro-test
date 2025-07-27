db667b5ba2f9abc0276ec9042468ed0934c16789
starytrek <stostostosto@gmail.com>
2025-07-27 23:03:56 +0200

    css problem

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import { loadEnv } from "vite";
+const { IMAGE_DOMAIN } = loadEnv(process.env.NODE_ENV ?? "development", process.cwd(), "");
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    domains: [IMAGE_DOMAIN],
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
