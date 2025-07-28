d32ece584b1aacf36a05695df66b2d9ec5a72a45
starytrek <stostostosto@gmail.com>
2025-07-28 18:13:30 +0200

    works well

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import tailwind from "@tailwindcss/vite";
 import { loadEnv } from "vite";
-const { IMAGE_DOMAIN } = loadEnv(process.env.NODE_ENV ?? "development", process.cwd(), "");
+const { IMAGE_DOMAIN } = loadEnv(
+  process.env.NODE_ENV ?? "development",
+  process.cwd(),
+  "",
+);
 // https://astro.build/config
 export default defineConfig({
   image: {
     domains: [IMAGE_DOMAIN],
   },
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwind()],
   },
 });
